47c30d5231c41735a48afc76a5045ecaa1476db0
ilkercelebi <ilkercelebi3@gmail.com>
2026-02-27 20:23:03 +0300

    1
    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -9,7 +9,7 @@ import TermsOfService from './pages/TermsOfService'
 
 function App() {
   return (
-    <Router basename="/Spendex">
+    <Router>
       <div className="bg-background-light dark:bg-background-dark font-display text-slate-900 dark:text-slate-100 selection:bg-primary/20 min-h-screen flex flex-col">
         <Header />
         <Routes>
